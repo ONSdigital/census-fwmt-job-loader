1abbe35fab3ea3640a5d576cfb120e6d97eee8bd
jacobharrison <jacob.harrison@qa.com>
2019-06-26 11:38:34 +0100

    inital commit

---
+++ src/main/java/uk/gov/ons/census/fwmt/jobloader/JobloaderApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.census.fwmt.jobloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobloaderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JobloaderApplication.class, args);
+  }
+
+}
